9434e02881cc7b5018a02ec76bd9bd41b8249e54
Özgür Özdemircili <ozdemircili@gmail.com>
2013-09-09 18:13:29 +0200

    Added hackernews api cheat

---
+++ pycheat/api_hackernews.py
@@ -0,0 +1,33 @@
+#Before anything install the Hackernews API
+from hn import HN
+
+hn = HN()
+
+
+# print top 10 stories from homepage
+for story in hn.get_stories()[:10]:
+    story.print_story()
+    print '*' * 50
+    print ''
+
+"""
+# print 10 latest stories
+for story in hn.get_stories(story_type='newest')[:10]:
+    story.print_story()
+    print '*' * 50
+    print ''
+
+# print all self posts from the homepage
+for story in hn.get_stories():
+    if story.is_self_post:
+        story.print_story()
+        print '*' * 50
+        print ''
+"""
+
+# print the top 10 stories from /best page
+for story in hn.get_stories(story_type='best')[:10]:
+    story.print_story()
+    print '*' * 50
+    print ''
+
